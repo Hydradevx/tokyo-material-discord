99eff5eadd6bdc8e6c3bb191158745ebf2d396ec
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-11-16 20:35:40 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tokyo-Material
+
+A sleek theme that brings you the style of Tokyo.
---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "Tokyo-Material",
+		author: "Hydradevx",
+		version: "0.0.1",
+		description: "A sleek theme that brings you the style of Tokyo.",
+		source: "https://github.com/Hydradevx/Tokyo-Material"
+	}
+}
